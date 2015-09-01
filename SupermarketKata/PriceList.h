d6a4082c28cd80065fe20551b4572d26a58019f8
unknown <9uv9ot@GBCBRBONES01.net.plm.eds.com>
2015-09-01 13:24:01 +0100

    Remove warning

--- SupermarketKata/PriceList.h
+++ SupermarketKata/PriceList.h
@@ -8,10 +8,10 @@ public:
 	PriceList();
 	~PriceList() {};
 
-	const std::string TinOfBeans() const { return std::string("TinOfBeans"); };
-	const std::string TinOfSoup() const { return std::string("TinOfSoup"); };
-	const std::string TinOfPeaches() const { return std::string("TinOfPeaches"); };
-	const std::string Coleslaw() const { return std::string("Coleslaw"); };
+	std::string TinOfBeans() const { return std::string("TinOfBeans"); };
+	std::string TinOfSoup() const { return std::string("TinOfSoup"); };
+	std::string TinOfPeaches() const { return std::string("TinOfPeaches"); };
+	std::string Coleslaw() const { return std::string("Coleslaw"); };
 
 	int operator [](const std::string& item);
 
